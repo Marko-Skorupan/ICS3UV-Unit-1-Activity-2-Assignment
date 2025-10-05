4b89de16a6bcc9234396e3aa71bda3bc7492f7ed
default <marko.skorupan@stu.ocsb.ca>
2025-10-05 22:03:30 +0000

    program that outputs the volume of a sphere with a radius of 4 cm

---
+++ program-3/main.ts
@@ -0,0 +1,10 @@
+/**
+ * @author Marko Skorupan
+ * @version 1.0.0
+ * @date 2025-10-05
+ * @fileoverview "program that outputs the volume of a sphere with a radius of 4 cm"
+ */
+
+// Calculate volume of shere
+// Radius = 4cm
+console.log("4 / 3 * 3.14 * 4 * 4 * 4 ="+String(4/3*3.14*4*4*4))
